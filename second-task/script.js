78084e0aefc6322e492ac0f8ccec34cdc3489993
Ansagan Islamgaliev <ansaganie@hotmail.com>
2022-05-05 19:23:09 +0600

    feat: Complete second task of the homework

---
+++ second-task/script.js
@@ -0,0 +1,24 @@
+const NUMBER_REGEX = /^\d+$/;
+const NUMBER_SYSTEM = 10;
+const NUMBERS_IN_WORDS = [ 'первое', 'второе' ];
+
+const values = [];
+
+for (let i = 0; i < 2; i++) {
+  const value = prompt(`Введите ${NUMBERS_IN_WORDS[i]} число'`).trim();
+
+  if (NUMBER_REGEX.test(value)) {
+    values.push(parseInt(value, NUMBER_SYSTEM))
+  } else {
+    alert('Некорректный ввод!');
+
+    break;
+  }
+}
+
+if (values.length === 2) {
+  const sum = values[0] + values[1];
+  const quotient = Math.trunc(values[0] / values[1]);
+
+  console.log(`Ответ: ${sum}, ${quotient}.`);
+}
